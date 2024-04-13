264b3537392b754987f076f93a1a3fc2d2bc74e0
Limbxbx <ilya.msd2003@gmail.com>
2024-04-13 15:56:19 +0300

    flake8 errors fixed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -23,7 +24,7 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         price = car.comfort_class * (self.clean_power - car.clean_mark) * (
-                    self.average_rating / self.distance_from_city_center)
+                self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car: object) -> object:
@@ -33,7 +34,8 @@ class CarWashStation:
 
     def rate_service(self, rating: int) -> float:
         total_ratings = self.count_of_ratings + 1
-        updated_rating = ((self.average_rating * self.count_of_ratings) + rating) / total_ratings
+        updated_rating = ((self.average_rating * self.count_of_ratings)
+                          + rating) / total_ratings
         self.average_rating = round(updated_rating, 1)
         self.count_of_ratings = total_ratings
         return self.average_rating
